88811b5d8d4f6b1576df025b6fb1a23c607f19f3
MzKyle <19862681939@163.com>
2025-04-10 21:52:53 +0800

    做打符弹道结算前的完整版

--- install/buff_tracker/include/buff_tracker/tracker.hpp
+++ install/buff_tracker/include/buff_tracker/tracker.hpp
@@ -1,6 +1,3 @@
-// Copyright (C) 2024 Zheng Yu
-// Licensed under the MIT License.
-
 #ifndef BUFF_TRACKER__TRACKER_HPP_
 #define BUFF_TRACKER__TRACKER_HPP_
 
@@ -33,7 +30,7 @@
 #include "buff_tracker/gauss_newton_solver.hpp"
 
 #define PI 3.1415926
-#define BLADE_R_OFFSET 700.0
+#define BLADE_R_OFFSET 700.0  //扇叶半径偏移量
 // #define OMEGA 1.0 / 3 * PI
 #define OMEGA 0.0
 namespace rm_buff
@@ -43,12 +40,13 @@ class Tracker
 public:
   Tracker(double max_match_theta, double max_match_center_xoy);
 
-  void init(const buff_interfaces::msg::BladeArray::SharedPtr & blades_msg);
+  void init(const buff_interfaces::msg::BladeArray::SharedPtr & blades_msg);//根据接收到的扇叶消息初始化跟踪器
 
-  void update(const buff_interfaces::msg::BladeArray::SharedPtr & blades_msg);
+  void update(const buff_interfaces::msg::BladeArray::SharedPtr & blades_msg);//根据接收到的扇叶消息更新跟踪器
 
-  void solve(const rclcpp::Time & time);
+  void solve(const rclcpp::Time & time);//在指定时间点进行求解
 
+  //三个求解器
   ExtendedKalmanFilter ekf;
 
   GaussNewtonSolver gns;
@@ -56,18 +54,18 @@ public:
   ExtendedKalmanFilter ekf_gns;
 
   double a_start, w_start, c_start;
-  double min_first_solve_time;
+  double min_first_solve_time;  //最小首次求解时间
 
-  int tracking_threshold;
-  int lost_threshold;
-  // bullet flight duration not enough ~ ms
-  int timeout_threshold;
+  //阈值参数
+  int tracking_threshold;//踪阈值，用于判断跟踪器是否进入跟踪状态。当满足一定条件（如连续检测到目标的次数）达到该阈值时，跟踪器进入跟踪状态。
+  int lost_threshold;//丢失阈值，用于判断跟踪器是否丢失目标。当连续未检测到目标的次数达到该阈值时，跟踪器认为目标丢失。
+  int timeout_threshold;//超时阈值，单位为毫秒（ms），子弹飞行时间不满足毫秒。当跟踪过程中的某些条件（如目标运动速度、距离等）导致子弹飞行时间不足时，跟踪器可能进入超时状态。
 
-  double blade_z_ground;
-  double robot_z_ground;
+  double blade_z_ground;//扇叶距离地面的高度
+  double robot_z_ground;//机器人距离地面的高度
   double distance;
-  double max_distance_diff;
-  double center_xoy_diff;
+  double max_distance_diff;//最大距离差值，用于判断目标的距离变化是否在合理范围内
+  double center_xoy_diff;//中心点在 x 和 y 平面上的差值，用于判断目标的位置变化是否在合理范围内
 
   enum State {
     LOST,
@@ -78,10 +76,10 @@ public:
   } tracker_state;
 
   enum SolverStatus {
-    WAITING,
-    NOT_ENOUGH_OBS,
-    VALID,
-    INVALID,
+    WAITING,//求解器正在等待足够的观测数据
+    NOT_ENOUGH_OBS,//观测数据不足，无法进行有效求解
+    VALID,//结果有效
+    INVALID,//结果无效
   } solver_status;
 
   // blade info
@@ -103,13 +101,13 @@ public:
 
   blade_transform tracked_blade;
 
-  Eigen::VectorXd measurement;
+  Eigen::VectorXd measurement; //测量值
 
-  Eigen::VectorXd target_state;
+  Eigen::VectorXd target_state; //目标状态
 
-  Eigen::VectorXd spd_state;
+  Eigen::VectorXd spd_state;  //速度状态
 
-  rclcpp::Time obs_start_time;
+  rclcpp::Time obs_start_time; //是否到达时间阈值
 
   void getTrackerPosition(blade_transform & blade);
 
@@ -118,7 +116,7 @@ private:
 
   blade_transform bladeTransform(const buff_interfaces::msg::Blade & blade);
 
-  bool handleBladeJump(double theta_diff);
+  bool handleBladeJump(double theta_diff);//处理扇叶角度跳跃的情况
 
   geometry_msgs::msg::Point rotateBlade(const blade_transform blade, int idx);
 
--- install/buff_tracker/include/buff_tracker/tracker_node.hpp
+++ install/buff_tracker/include/buff_tracker/tracker_node.hpp
@@ -1,6 +1,3 @@
-// Copyright (C) 2024 Zheng Yu
-// Licensed under the MIT License.
-
 #ifndef BUFF_TRACKER__TRACKER_NODE_HPP_
 #define BUFF_TRACKER__TRACKER_NODE_HPP_
 
@@ -43,29 +40,35 @@ public:
 
 private:
   void bladesCallback(const buff_interfaces::msg::BladeArray::SharedPtr blades_msg);
-  double dt_;
+  double dt_;//时间间隔，用于计算时间差
 
-  rclcpp::Time last_time_;
+  rclcpp::Time last_time_;//上次接收数据的时间
 
-  double lost_time_threshold_;
+  double lost_time_threshold_;//丢失时间阈值，用于判断是否丢失目标
 
   std::unique_ptr<Tracker> tracker_;
 
   // param
+  //噪声
   double s2qxyz_;
   double s2qtheta_;
   double s2qr_;
   double r_blade_;
   double r_center_;
 
-  int max_iter_;
-  double min_step_;
-  int obs_max_size_;
+  int max_iter_;//表示最大迭代次数。在使用迭代算法（如高斯牛顿法）求解问题时，为了避免无限循环，会设置一个最大迭代次数，当达到这个次数时，算法停止迭代。
+  double min_step_;//表示最小步长。在迭代过程中，如果每次迭代的步长小于这个值，说明算法已经收敛到一个较优的解，此时可以停止迭代。
+  int obs_max_size_;//表示观测值的最大数量。在跟踪过程中，可能会存储一定数量的观测值用于后续的处理和分析，这个参数限制了观测值的最大存储数量
+
+  //分别表示加速度的最大值和最小值。在跟踪目标的运动过程中，为了避免不合理的加速度值，会设置一个合理的范围
   double max_a_;
   double min_a_;
+
+  //分别表示角速度的最大值和最小值。同样，为了避免不合理的角速度值，会设置一个合理的范围。
   double max_w_;
   double min_w_;
 
+  //协方差噪声
   double s2q_a_;
   double s2q_w_;
   double s2q_c_;
@@ -74,8 +77,8 @@ private:
   double r_c_;
 
   //  task subscriber
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr task_sub_;
-  std::string task_mode_;
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr task_sub_; //任务订阅者，用于订阅任务模式消息
+  std::string task_mode_;//当前的任务模式
   void taskCallback(const std_msgs::msg::String::SharedPtr task_msg);
 
   // Subscriber with tf2 message_filter
